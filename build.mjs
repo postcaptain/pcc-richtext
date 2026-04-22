546cd5b3b420acbd6688eb2813121a5558347041
Jake Daly <47786887+jacobtdaly@users.noreply.github.com>
2026-04-22 14:15:18 -0400

    Add build script using esbuild for bundling

---
+++ build.mjs
@@ -0,0 +1,14 @@
+import * as esbuild from 'esbuild';
+import { readFile } from 'node:fs/promises';
+
+const pkg = JSON.parse(await readFile(new URL('./package.json', import.meta.url), 'utf8'));
+
+await esbuild.build({
+	entryPoints: ['src/index.js'],
+	bundle: true,
+	format: 'iife',
+	outfile: 'dist/pcc-richtext.js',
+	define: {
+		__VERSION__: JSON.stringify(pkg.version)
+	}
+});
