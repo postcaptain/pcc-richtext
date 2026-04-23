15e0b2bc70424d1da4b2dc8774325b8655e6598f
Jake Daly <47786887+jacobtdaly@users.noreply.github.com>
2026-04-23 17:47:40 -0400

    Update package.json path in build script

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,7 +1,7 @@
 import * as esbuild from 'esbuild';
 import { readFile } from 'node:fs/promises';
 
-const pkg = JSON.parse(await readFile(new URL('./package.json', import.meta.url), 'utf8'));
+const pkg = JSON.parse(await readFile(new URL('../package.json', import.meta.url), 'utf8'));
 
 await esbuild.build({
 	entryPoints: ['src/index.js'],
